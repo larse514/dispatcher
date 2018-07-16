1a9758729fe553e3506d14f4b6b550c0e39609a4
larse514 <andrew.larsen@concordusa.com>
2018-07-16 00:55:23 -0500

    add correct url

--- dispatch/sourceclient.go
+++ dispatch/sourceclient.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 //AWSSource is an implemenation of MessageDispatcher
@@ -22,13 +23,16 @@ type SourceDTO struct {
 
 //GetRoutes is a method to retrieve routes for a source
 func (source AWSSource) GetRoutes(sourcename string) ([]Route, error) {
-	req, err := http.NewRequest("GET", source.URL, nil)
+
+	url := strings.Replace(source.URL, ":name", sourcename, -1)
+	log.Println("DEBUG: created url ", url)
+	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := source.Client.Do(req)
 
 	if err != nil {
-		log.Println("ERROR: error making http call ", err)
+		log.Println("ERROR: error making http call to source with error", err)
 		return nil, errors.New("error making http call")
 	}
 
