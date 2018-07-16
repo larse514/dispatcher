eeecef137d11c98932b8edc5a21266f6e284b75a
larse514 <andrew.larsen@concordusa.com>
2018-07-16 00:26:04 -0500

    get url to add to lambda

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,13 @@ set -e
 
 STACK_NAME=$1
 DEPLOYMENT_BUCKET=$2
+API_STACK=$3
 
-make DEPLOYMENT_BUCKET=${DEPLOYMENT_BUCKET}
-make package-aws DEPLOYMENT_BUCKET=${DEPLOYMENT_BUCKET}
+APIURL=`aws cloudformation describe-stacks \
+            --stack-name $API_STACK \
+            --query "Stacks[0].Outputs[0].{OutputValue:OutputValue}" \
+            --output text`
+
+make
+make package-aws DEPLOYMENT_BUCKET=${DEPLOYMENT_BUCKET} SOURCE_URL=${APIURL}
 make deploy STACK_NAME=${STACK_NAME} ENV=ci 
